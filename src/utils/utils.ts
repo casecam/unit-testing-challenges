a2ec53a1ea7fb9613ba67c604035bf3086f64704
caseycameron <mcasey.cameron@gmail.com>
2022-07-28 11:14:08 -0700

    Refactor - sum of all prices test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,5 @@
 import { getRandomItems } from '../data/data'
 
 export function getTotalPrice(count: number) {
-  // const items = getRandomItems(count)
-
   return getRandomItems(count).reduce((sum, { amount, price }) => sum + price * amount, 0)
 }
