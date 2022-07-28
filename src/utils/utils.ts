4ce1290a3ff099037fdf8f7994593167329f0234
caseycameron <mcasey.cameron@gmail.com>
2022-07-28 11:03:41 -0700

    Red - sum totals test

---
+++ src/utils/utils.ts
@@ -0,0 +1,7 @@
+import { getRandomItems } from '../data/data'
+
+export function getTotalPrice(count: number) {
+  // const items = getRandomItems(count)
+
+  return getRandomItems(count).reduce((sum, { amount, price }) => sum + price - amount, 0)
+}
