3ba70eb29483b011ffd9b073fb841f9041ade8b9
caseycameron <mcasey.cameron@gmail.com>
2022-07-28 11:49:08 -0700

    RED - return 1st obj that matches property value

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,11 +1,27 @@
-import { getTotalPrice } from "./utils";
-
+import { dummyArray } from "../data/data";
+import { getTotalPrice, matchObjectAndString } from "./utils";
 
 describe('unit tests', () => {
   it('takes an array of objects & returns sum of all prices', () => {
     const numOfItems = 5
-    const expected = 150
     const actual = getTotalPrice(numOfItems)
+    const expected = 150
     expect(expected).toEqual(actual) 
+  })
+
+  it('takes an array & string & returns 1st object that matches property\'s value & string', () => {
+    const stringToMatch = 'MATCH_THIS_VALUE'
+    const actual = matchObjectAndString(dummyArray, stringToMatch)
+    const expected =   {
+      id: 2,
+      name: 'MATCH_THIS_VALUE',
+      amount: 2,
+      price: 15.00,
+      description: expect.any(String),
+      inStock: true
+    }
+    console.log('expected', expected, 'actual', actual);
+    
+    expect(actual).toMatchObject(expected)
   })
 })
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,10 @@
 import { getRandomItems } from '../data/data'
+import { Item } from '../data/types'
 
 export function getTotalPrice(count: number) {
   return getRandomItems(count).reduce((sum, { amount, price }) => sum + price * amount, 0)
+}
+
+export function matchObjectAndString(arr: Item[], stringToMatch: string) {
+  return {}
 }
