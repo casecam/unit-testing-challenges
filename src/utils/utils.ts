d25f03e3445cd798361683036bf1c222c2049fad
caseycameron <mcasey.cameron@gmail.com>
2022-07-28 11:03:55 -0700

    Green - sum totals test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,5 +3,5 @@ import { getRandomItems } from '../data/data'
 export function getTotalPrice(count: number) {
   // const items = getRandomItems(count)
 
-  return getRandomItems(count).reduce((sum, { amount, price }) => sum + price - amount, 0)
+  return getRandomItems(count).reduce((sum, { amount, price }) => sum + price * amount, 0)
 }
