0d8ef4a0cf46a8c8fdc7eaa1a4a2877841fb4126
caseycameron <mcasey.cameron@gmail.com>
2022-07-29 11:38:36 -0700

    Refactor - better SWAPI interfaces

--- src/data/types.ts
+++ src/data/types.ts
@@ -7,6 +7,32 @@ export type Item = {
   inStock: boolean;
 }
 
+export interface SWAPIResponse  {
+  "count": number,
+  "next": string | null,
+  "previous": string | null,
+  "results": SWAPIResponseObject[]
+}
+
+export interface SWAPIResponseObject {
+    "name": string, 
+    "height": "172", 
+    "mass": "77", 
+    "hair_color": string, 
+    "skin_color": string, 
+    "eye_color": string, 
+    "birth_year": string, 
+    "gender": string, 
+    "homeworld": string, 
+    "films": string[], 
+    "species": [], 
+    "vehicles": string[],
+    "starships": string[], 
+    "created": string,
+    "edited": string, 
+    "url": string,
+}
+
 export type JSONValue =
 | string
 | number
